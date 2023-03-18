4068d51fd93a9ade3e5c7ef0d35af8794518a73f
Rodion <Rodion.Smolnikov@gmail.com>
2023-03-18 15:26:13 +0300

    change paint function with params main class width

--- js/tablemade script.js
+++ js/tablemade script.js
@@ -1,37 +1,44 @@
-const table=document.getElementById("rodia_ti_zloy")
+/*
+порядок применения стилей
+1) Стиль вписанный в элемент html
+2) Стиль считанный в id
+3) Стиль считанный в class
+4) !
+5) *
+TODO: Разобраться в порядке применения стилей
+*/
 
+const ladder = document.getElementById("rodia_ti_zloy")
+const colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+                        '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+                        '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+                        '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+                        '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+                        '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+                        '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+                        '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+                        '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+                        '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF', '#4DB3FF'];
 
-const fifty_spartans =()=>{
-
-        const colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-                            '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-                            '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
-                            '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-                            '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
-                            '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-                            '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
-                            '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-                            '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
-                            '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF', '#4DB3FF'];
-        const height = new Array ()
-        for (i=10 ; i <160; i+=2.8){
-            height.push(10+i)
-        }                  
-    // 50 columns
-    for ( i=1; i<51; i++) {
-    // let b = document.createElement("td")
-    let b = document.createElement("div")
-    column=table.insertAdjacentElement("beforeend", b )
-    column.setAttribute("id","t"+i, )
-    column.setAttribute("class", "td_"+i)
-    column.style.backgroundColor = colorArray[i]
-    column.style.height = height[i]
-    }
-    // add id
+function fifty_spartans(ladder, n) {
+    const heights = []
+    for (i = 0 ; i < n; i ++) {
+        heights[i] = 10 + i*2.8
+    }                  
     
-    // let totalRowCount = table.rows.length; // 5
-    // let tbodyRowCount = table.tBodies[0].rows.length
-
+    for (i = 1; i < n+1; i++) {
+        let box = document.createElement("div")
+        column = ladder.insertAdjacentElement("beforeend", box)
+        
+        column.setAttribute("id","col_" + i)
+        column.style.backgroundColor = colorArray[(i-1) % colorArray.length] //от 0 до colorArray.length-1
+        column.style.height = heights[i-1]
+    }
 }
 
-fifty_spartans()
+// 24 разделить нацело на 50 == 0*50 + 24
+// 0 разделить нацело на 50 == 0*50 + 0
+// 48 разделить нацело на 50 == 0*50 + 48
+// 51 разделить нацело на 50 == 1*50 + 1
+
+fifty_spartans(ladder, 60)
