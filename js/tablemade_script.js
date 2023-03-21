9fc5eb0719b02e44fb0e5629400a244600b8ea03
pomPusha <ermax0491@mail.ru>
2023-03-21 11:15:11 +0000

    счся

--- js/tablemade_script.js
+++ js/tablemade_script.js
@@ -16,7 +16,7 @@
 
 Задание 3:
 Написать фунцию shuffle, которая будет по кнопке перемешивать cтупеньки.
-Это кнопка должна быть ниже поля.
+Это кнопка должна быть ниже поля. DONE
 
 Задание 4:
 Добавить кнопку Grey внутрь внешнего бокса - перекрасить все столбцы в серый цвет.
@@ -28,37 +28,151 @@
 И кнопкой применить.
 
 */
+//
+const width_input = document.getElementById("input_width");
+const button_grey = document.getElementById("button_grey");
+const ladder = document.getElementById("rodia_ti_zloy");
+const fiftin_spartains = document.getElementsByClassName("grey");
 
-const ladder = document.getElementById("rodia_ti_zloy")
-const colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-                        '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-                        '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
-                        '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-                        '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
-                        '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-                        '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
-                        '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-                        '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
-                        '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF', '#4DB3FF'];
+const colorArray = [
+  "#FF6633",
+  "#FFB399",
+  "#FF33FF",
+  "#FFFF99",
+  "#00B3E6",
+  "#E6B333",
+  "#3366E6",
+  "#999966",
+  "#99FF99",
+  "#B34D4D",
+  "#80B300",
+  "#809900",
+  "#E6B3B3",
+  "#6680B3",
+  "#66991A",
+  "#FF99E6",
+  "#CCFF1A",
+  "#FF1A66",
+  "#E6331A",
+  "#33FFCC",
+  "#66994D",
+  "#B366CC",
+  "#4D8000",
+  "#B33300",
+  "#CC80CC",
+  "#66664D",
+  "#991AFF",
+  "#E666FF",
+  "#4DB3FF",
+  "#1AB399",
+  "#E666B3",
+  "#33991A",
+  "#CC9999",
+  "#B3B31A",
+  "#00E680",
+  "#4D8066",
+  "#809980",
+  "#E6FF80",
+  "#1AFF33",
+  "#999933",
+  "#FF3380",
+  "#CCCC00",
+  "#66E64D",
+  "#4D80CC",
+  "#9900B3",
+  "#E64D66",
+  "#4DB380",
+  "#FF4D4D",
+  "#99E6E6",
+  "#6666FF",
+  "#4DB3FF",
+];
+let width = 4;
+// создание 50 дивов
 
-function fifty_spartans(ladder, n) {
-    const heights = []
-    for (i = 0 ; i < n; i ++) {
-        heights[i] = 10 + i*2.8
-    }                  
-    
-    for (i = 1; i < n+1; i++) {
-        let box = document.createElement("div")
-        column = ladder.insertAdjacentElement("beforeend", box)
-        
-        column.setAttribute("id","col_" + i)
-        column.style.backgroundColor = colorArray[(i-1) % colorArray.length] //от 0 до colorArray.length-1
-        column.style.height = heights[i-1]
+// console.log(width);
+function fifty_spartans(ladder, n, width_) {
+  const heights = [];
+  for (i = 0; i < n; i++) {
+    heights[i] = 10 + i * 2.8;
+  }
+
+  for (i = 1; i < n + 1; i++) {
+    let box = document.createElement("div");
+    column = ladder.insertAdjacentElement("beforeend", box);
+
+    column.setAttribute("id", "col_" + i);
+    column.setAttribute("class", "grey");
+    column.style.backgroundColor = colorArray[(i - 1) % colorArray.length];
+    column.style.height = heights[i - 1];
+    console.log(width_);
+    column.style.width = width_;
+  }
+}
+
+// кнопка изменение цвета
+function grey() {
+  if (button_grey.value === "grey") {
+    button_grey.value = "Rainbow";
+    for (i in fiftin_spartains) {
+      fiftin_spartains[i].style.backgroundColor =
+        colorArray[i % colorArray.length];
     }
+  } else if (button_grey.value === "Rainbow") {
+    button_grey.value = "grey";
+
+    for (i in fiftin_spartains) {
+      fiftin_spartains[i].style.backgroundColor = "grey";
+      fiftin_spartains[i].style.border = "black";
+      fiftin_spartains[i].style.borderWidth = "0.1";
+    }
+  }
 }
 
-function shuffle() {
-    alert("I'm not implemented")
+// кнопка перемешивания столбиков
+const shuffle = () => {
+  // console.log(fiftin_spartains);
+  let arr_fiftin_spartains = [].slice.call(fiftin_spartains);
+  while (ladder.hasChildNodes()) {
+    ladder.removeChild(ladder.firstChild);
+  }
+  let shiffeled_arr_fiftin_spartains = [];
+  let shiffeled2_arr_fiftin_spartains = [];
+  arr_fiftin_spartains.forEach((div_element) => {
+    if (Math.round(Math.random()) === 1) {
+      shiffeled_arr_fiftin_spartains.push(div_element);
+    } else {
+      shiffeled2_arr_fiftin_spartains.push(div_element);
+    }
+    arr_result = [
+      ...shiffeled_arr_fiftin_spartains,
+      ...shiffeled2_arr_fiftin_spartains,
+    ];
+    arr_result.forEach((el) => ladder.appendChild(el));
+  });
+  // console.log(arr_fiftin_spartains);
+};
+
+// установка размеров рамки
+function chanjeSize(object, width_) {
+  document.getElementById(object).style.width = 10 * 50;
+  console.log();
 }
 
-fifty_spartans(ladder, 50)
+chanjeSize("f1", 4);
+fifty_spartans(ladder, 50, width);
+
+// test INPUT
+let some = [];
+function tryn() {
+  // ;
+  a = width_input.value;
+  alert(typeof a);
+  // // array.forEach(element => {
+  //   while (ladder.hasChildNodes()) {
+  //   //   ladder.removeChild(ladder.firstChild);
+  //   // }
+  // });
+  // fifty_spartans(ladder, 50, width_input.value);
+}
+console.log(some);
