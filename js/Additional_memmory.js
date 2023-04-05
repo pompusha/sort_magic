0f6ae00d3dcbfb61431ee19952e2546d70ea7419
RodionSmolnikov <rodion.smolnikov@gmail.com>
2023-04-05 12:30:13 +0300

    Update Additional_memmory.js

--- js/Additional_memmory.js
+++ js/Additional_memmory.js
@@ -1,4 +1,6 @@
-import { width } from "./tablemade_script";
+//Чтобы что-то импортировать - нужно это что-то сначала экспортировать.
+//Вот тут можно прочитать https://learn.javascript.ru/import-export
+import { width } from "./tablemade_script"; // Ну и файл у тебя называется с не "tablemade_script" а "tablemade_script.js"
 
 console.log("add con");
 console.log("width");
